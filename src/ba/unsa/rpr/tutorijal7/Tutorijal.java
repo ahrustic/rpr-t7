87766c80427664814f760eba4e7b5981e0a2872f
Amila Hrustić <ahrustic2@etf.unsa.ba>
2018-12-03 23:59:08 +0100

    dodana datoteka i drzave.xml

--- src/ba/unsa/rpr/tutorijal7/Tutorijal.java
+++ src/ba/unsa/rpr/tutorijal7/Tutorijal.java
@@ -1,6 +1,15 @@
 package ba.unsa.rpr.tutorijal7;
 
+import java.util.ArrayList;
+
 public class Tutorijal {
+
+    static ArrayList<Grad> ucitajGradove(){
+        ArrayList<Grad> gradovi = new ArrayList<>();
+
+        return gradovi;
+    }
+
 
     public static void main(String[] args) {
 	// write your code here
