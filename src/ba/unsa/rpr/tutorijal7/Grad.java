0390460ccd8aef6375c5101f068880ae3bcc72e8
Amila Hrustić <ahrustic2@etf.unsa.ba>
2018-12-04 00:02:42 +0100

    dodani konstruktori u gradu

--- src/ba/unsa/rpr/tutorijal7/Grad.java
+++ src/ba/unsa/rpr/tutorijal7/Grad.java
@@ -7,6 +7,21 @@ public class Grad implements Serializable {
     String naziv;
     int brojStanovnika;
     double[] temperature = new double[1000];
+    int duzinaNiza;
+
+    public Grad(){
+        duzinaNiza = 0;
+        temperature = new double[1000];
+    }
+
+    public Grad(String grad, Double[] temperature) {
+        naziv = grad;
+        duzinaNiza = temperature.length;
+        this.temperature = new double[1000];
+        for (int i = 0; i < duzinaNiza; i++)
+            this.temperature[i] = temperature[i];
+        brojStanovnika = 0;
+    }
 
     public String getNaziv() {
         return naziv;
